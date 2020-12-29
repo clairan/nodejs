23776a36f549168cf841be6c786b879da74edede
Andreas Clair <andreas.clair@gmail.com>
2020-12-29 22:01:58 +0100

    basics

--- myfirst.js
+++ myfirst.js
@@ -1,12 +1,13 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var events = require('events');
 
 http.createServer(function(req, res) {
     fs.readFile('demofile1.html', function(err, data){
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
-      res.end()
+        res.end();
 
     });
     
